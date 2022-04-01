f3435ae92b4920668e5562d8a3dd8f64edd14a8a
Ryohei Fujihara <giver.yell@gmail.com>
2022-04-02 08:09:54 +0900

    config #1

---
+++ ruby/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ad88d4b01a4ba7e3f9fb0f85438db78df6a7b601a6592dbec9434a4360853d4e78e8ca3343364b844c5a1a903e1b2ec75f05ae74d6cc10fae5fa75ff444f119d'
