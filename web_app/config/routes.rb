192d76c5da2ff1468d9cb3fce3a2d3bfb9a02db6
Ryohei Fujihara <giver.yell@gmail.com>
2022-04-04 09:32:37 +0900

    編集画面 #3

--- web_app/config/routes.rb
+++ web_app/config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  get 'todolists/new'
-
+  # top画面
+  get  'top' => 'homes#top'
+  # 投稿画面
+  get  'todolists/new'
+  # 投稿処理
   post 'todolists' => 'todolists#create'
+  # 一覧画面
   get  'todolists' => 'todolists#index'
+  # 詳細画面
+  get  'todolists/:id' => 'todolists#show', as: 'todolist'
+  # 編集画面
+  get  'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+  # 編集処理
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
