e249897c18eeca840206e67f2d326920b09de580
Ryohei Fujihara <giver.yell@gmail.com>
2022-04-05 09:14:45 +0900

    Refile.secret_key追加 #3

--- web_app/config/initializers/application_controller_renderer.rb
+++ web_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f9c60abb4b90bd89a609f739d85b2b9bc3bb012c99efe9438a42145bb671239d96143099f70c79a7abe1e497784a5a50d9ca2d1916bd32f430fcebe64926a488'
