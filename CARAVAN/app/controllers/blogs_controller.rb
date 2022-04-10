021f1a51ca3b4722f28ec35d16be11bdf8bfa711
Ryohei Fujihara <giver.yell@gmail.com>
2022-04-11 07:53:52 +0900

    投稿機能 #5

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -6,8 +6,24 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def edit
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
+  end
+
+  def edit
+  end
+
+  private
+  def blog_params
+    # params.require(:モデル名).permit(:カラム名1, :カラム名2, ...)
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
